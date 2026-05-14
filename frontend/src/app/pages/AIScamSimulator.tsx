69ce59ac4c301c10dbdc3b5d262e331ae157c1f3
SaumyaBelgi <saumya.belgi@gmail.com>
2026-05-14 12:20:03 +0530

    Fixed urls for deployment

--- frontend/src/app/pages/AIScamSimulator.tsx
+++ frontend/src/app/pages/AIScamSimulator.tsx
@@ -1,14 +1,12 @@
-
 export function AIScamSimulator() {
+  const backendUrl = import.meta.env.VITE_API_URL;
   return (
-    
     <div className="w-full h-[calc(100vh-64px)]">
       <iframe
-        src="http://localhost:5000"
+        src={backendUrl}
         className="w-full h-full border-none"
         title="AI Scam Simulator"
       />
     </div>
-    
   );
 }
